4729a64642e0de6423c67a5a28f76f025c5db88d
henrique-furtado47 <henrique.furtadoifc@gmail.com>
2026-06-11 11:15:12 -0300

    style(ui/button): replace spinner SVG with Font Awesome spinner icon

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -35,15 +35,7 @@ const classes = computed(() => [
 
 <template>
   <button :type="type" :class="classes" :disabled="disabled || loading">
-    <svg
-      v-if="loading"
-      class="h-4 w-4 animate-spin"
-      viewBox="0 0 24 24"
-      fill="none"
-    >
-      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z" />
-    </svg>
+    <font-awesome-icon v-if="loading" :icon="['fas', 'spinner']" class="h-4 w-4 animate-spin" />
     <slot />
   </button>
 </template>
